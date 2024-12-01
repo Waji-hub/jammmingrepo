de2a0d92f63300425ef3a5f377d06f4f2bd8dcce
Waji-hub <fagbuaroboluwaji@yahoo.com>
2024-12-01 01:01:42 +0000

    I made a mistake with the 'Jamming' naming instead of 'jammming'.

---
+++ src/JammmingApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function JammingApp(){
+    return (
+        <div>
+        <button>
+            Save to Spotify
+        </button>
+        <button>
+            Save
+        </button>
+        </div>
+      
+    )
+}
+export default JammingApp;
